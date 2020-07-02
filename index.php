e534856127ec2ca5f0b5c361fc23a8be1a606949
Lucien Mary <marylucien1998@gmail.com>
2020-07-02 22:00:42 +0200

    Nvlle classification scss + ajout bootstrap

--- index.php
+++ index.php
@@ -6,20 +6,29 @@
         <meta name="theme-color" content="#292929">
 
         <title>Accueil | lucienmary.be</title>
+
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="dist/assets/css/app.min.css">
     </head>
     <body>
-        <header>
+        <header class="container">
             <div class="">
                 <h2>Ml</h2>
             </div>
         </header>
-        <section>
+
+        <section class="container">
             <header>
                 <h1>Lucien Mary</h1>
-                <p><strong>Junior web developer</strong></p>
+                <p class="line"><strong>Junior web developer</strong></p>
             </header>
             <p>Je m’appelle Lucien Mary, j’ai 22 ans. Après une qualification en infographie, j'ai été diplomé en design web et mobile à la Haute École Albert Jacquard en 2020. Vous pouvez retrouver sur ce site mes meilleurs projets.</p>
             <img src="" alt="Photo portrait de Lucien Mary">
+
+            <a href="#">Projets</a>
+
             <footer>
                 <ul>
                     <li>
@@ -38,7 +47,19 @@
                         </a>
                     </li>
                 </ul>
+
+                <p><a href="mailto:marylucien1998@gmail.com">marylucien1998@gmail.com</a></p>
+                <p><a href="tel:+32479528839">+32 479 528 839</a></p>
             </footer>
         </section>
+
+        <footer class="container">
+            <p>© 2020 - Lucien Mary</p>
+        </footer>
+
+        <!-- Scripts. -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     </body>
 </html>
